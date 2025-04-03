42be57ceef8090934f84212067c0e1dd08f06c3e
deltron-fr <kayceeogbonnaya2304@gmail.com>
2025-04-03 16:01:31 +0100

    Fixed the nginx ssl issue

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -6,32 +6,11 @@ resource "local_file" "inventory" {
     EOF
 }
 
-resource "null_resource" "dns_provisioner"{
-  provisioner "remote-exec" {
-    inline = [ "sudo curl -u 'deltronfr:${var.dns_password}' http://@freedns.afraid.org/nic/update?hostname=deltronfr.mooo.com&myip=${azurerm_linux_virtual_machine.main.public_ip_address}",]
-  
-    connection {
-      host = "${azurerm_linux_virtual_machine.main.public_ip_address}"
-      type = "ssh"
-      user = var.admin_username
-      private_key = file(var.ssh_pri_key)
-    }
-  }
-
-  triggers = {
-    script_version = "1.0.1" 
-  }
-
-  depends_on = [ azurerm_linux_virtual_machine.main,
-                 azurerm_public_ip.public_ip,
-                 azurerm_network_security_group.nsg,
-                 local_file.inventory
-               ]
-}
 
 resource "null_resource" "ansible_provisioner" {
   provisioner "remote-exec" {
-    inline = [ "sudo apt update -y", "echo Done!" ]
+    inline = [ "sudo apt update -y", "echo Done!",
+               "curl http://'deltronfr:${var.dns_password}'@freedns.afraid.org/nic/update?hostname=deltronfr.mooo.com&myip=${azurerm_linux_virtual_machine.main.public_ip_address} && echo ip changed successfully" ]
 
     connection {
       host = "${azurerm_linux_virtual_machine.main.public_ip_address}"
